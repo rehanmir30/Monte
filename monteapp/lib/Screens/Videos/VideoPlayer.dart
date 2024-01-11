a791d6b6814630ab17dca8439b75da4803f5cc21
rehanmir30 <52622422+rehanmir30@users.noreply.github.com>
2024-01-11 19:32:48 +0500

    updated code before in app purchases

--- monteapp/lib/Screens/Videos/VideoPlayer.dart
+++ monteapp/lib/Screens/Videos/VideoPlayer.dart
@@ -196,7 +196,6 @@ class _VideoPlayerState extends State<VideoPlayer> {
                             ],
                           ),
                         ),
-
                       ],
 
                     ),
